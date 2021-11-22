1c5e3582dcf0001d9ca3d910ef8036034088fe94
Alireza H <Aliireza_H@protonmail.com>
2021-11-22 23:48:17 +0330

    Added cheating Button.

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,8 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+  int lDiceNumber = 1;
+  int rDiceNumber = 1;
 
   void changeDiceFace() {
     setState(() {
@@ -34,40 +36,76 @@ class _DicePageState extends State<DicePage> {
     });
   }
 
+  void cheatDiceFace() {
+    lDiceNumber = Random().nextInt(6) + 1;
+    rDiceNumber = Random().nextInt(6) + 1;
+    if (lDiceNumber + rDiceNumber >= rightDiceNumber + leftDiceNumber) {
+      setState(() {
+        rightDiceNumber = rDiceNumber;
+        leftDiceNumber = lDiceNumber;
+      });
+    } else {
+      cheatDiceFace();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                child: Image.asset(
-                  'images/dice$leftDiceNumber.png',
-                ),
-                onPressed: () {
-                  changeDiceFace();
-                },
+    return Column(
+      children: [
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              splashFactory: NoSplash.splashFactory,
+            ),
+            onPressed: () {
+              changeDiceFace();
+            },
+            child: Center(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        'images/dice$leftDiceNumber.png',
+                      ),
+                    ),
+                  ),
+                  //Get students to create the second die as a challenge
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        'images/dice$rightDiceNumber.png',
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          //Get students to create the second die as a challenge
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                child: Image.asset(
-                  'images/dice$rightDiceNumber.png',
-                ),
-                onPressed: () {
-                  changeDiceFace();
-                },
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 200.0, bottom: 70.0),
+          child: SizedBox(
+            width: 80.0,
+            height: 80.0,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                splashFactory: NoSplash.splashFactory,
+              ),
+              onPressed: () {
+                cheatDiceFace();
+              },
+              child: SizedBox(
+                width: 80.0,
+                height: 80.0,
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
